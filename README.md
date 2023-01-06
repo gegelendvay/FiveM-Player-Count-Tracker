cd2c2c806ea8f6b816a69ec56e0005bc0f5705c9
Gergely Lendvay <gege.lendvay2003@gmail.com>
2023-01-07 00:31:16 +0100

    Average player count, color config

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ Before running the application, make sure to install the required python modules
 - Requests - `pip install requests`
 - Matplotlib - `pip install matplotlib`
 
-Once you are done installing the required modules, set up the servers you wish to track. Find the `servers` array, and add an array containing the server name and connection IP: `["Example Server", "127.0.0.1"]`
+Once you are done installing the required modules, set up the servers you wish to track. Find the `servers` array, and add an array containing the server name, connection IP and a color of your choice: `["Example Server", "127.0.0.1", "blue"]`
 
 If the IPv4 address is hidden behind a reverse proxy, open a terminal, and write the following: `ping exampleserver.com`
 
 ## Updates
 Ideas on how to further improve the program can be shared using the Issues or Pull Requests tabs in the repository page.
 - [ ] Auto refresh
+- [ ] Use of FiveM server ID instead of server IP
 
 ## License
 The project is licensed under the MIT License
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import requests as req
 import matplotlib.pyplot as plt
+import requests as req
 
 servers = [
-    ["Example Server 1", "127.0.0.1"],
-    ["Example Server 2", "127.0.0.1"],
-    ["Example Server 3", "127.0.0.1"],
+    ["Example Server 1", "127.0.0.1", "b"],
+    ["Example Server 2", "127.0.0.1", "green"],
+    ["Example Server 3", "127.0.0.1", "#9128f6"],
 ]
 
 plt.figure(num="FiveM Player Count Tracker")
 plt.title("FiveM Player Count Tracker")
 
 for i in servers:
-    xAxis = []
-    yAxis = []
+    xAxis, yAxis = [], []
     try:
-        server = i[0]
+        server, color = i[0], i[2]
         res = req.get(f"http://{i[1]}:40120/chartData/svMain").json()
-        xAxis = (list(range(1, len(res)+1)))
+        xAxis = (list(range(0, len(res))))
         for i in res:
             yAxis.append(i["clients"])
-        plt.plot(xAxis, yAxis, label=server)
+        average = round(sum(yAxis)/len(yAxis), 2)
+        plt.plot(xAxis, yAxis, label=f'{server} - {average}', color=color)
     except Exception as e:
         print(e)
 
+plt.xlabel("Time ~ 30h")
+plt.ylabel("Player Count")
 plt.legend()
 plt.show()
